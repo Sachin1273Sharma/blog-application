6fe05f2f262b483e033aaf2d732dfaaf68117a1e
Sachin Sharma <sachinsharma81494@gmail.com>
2024-09-06 14:04:41 +0530

    update post functionality added

--- src/main/java/io/mountblue/blog_application_project/controller/PostController.java
+++ src/main/java/io/mountblue/blog_application_project/controller/PostController.java
@@ -2,6 +2,7 @@ package io.mountblue.blog_application_project.controller;
 
 import io.mountblue.blog_application_project.entity.Comment;
 import io.mountblue.blog_application_project.entity.Post;
+import io.mountblue.blog_application_project.entity.Tag;
 import io.mountblue.blog_application_project.service.PostService;
 import io.mountblue.blog_application_project.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class PostController {
@@ -50,5 +53,31 @@ public class PostController {
         model.addAttribute("commentList", commentList);
         model.addAttribute("commentForm", new Comment());
         return "post";
+    }
+    @GetMapping("/update/{id}")
+    public String showUpdatePage(@PathVariable("id") Long id, Model model) {
+        Post post = postService.getPostById(id);
+        Set<Tag> tags = post.getTags();
+        List<String> tagNames = new ArrayList<>();
+        for (Tag tag : tags) {
+            tagNames.add(tag.getName());
+        }
+        String tagsInput = String.join(",", tagNames);
+        model.addAttribute("post", post);
+        model.addAttribute("tagsInput", tagsInput);
+        return "update-post";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updatePost(@PathVariable("id") Long id, @ModelAttribute("post") Post post, @RequestParam("tagsInput") String tags) {
+        Post oldPost = postService.getPostById(id);
+        oldPost.setTitle(post.getTitle());
+        oldPost.setUpdatedAt(LocalDateTime.now());
+        oldPost.setAuthor(post.getAuthor());
+        oldPost.setContent(post.getContent());
+        oldPost.setExcerpt(post.getExcerpt());
+        postService.savePost(oldPost);
+        tagService.addTagsToPost(post.getId(), tags, true);
+        return "redirect:/posts/" + post.getId();
     }
 }
